48b80bc0b0e6fa56ad5a5d72f3108c55ac9cf21e
yan404dev <yancastrohenrique1@gmail.com>
2025-12-31 18:39:03 -0300

    fix cors

--- packages/web/src/shared/services/http.service.ts
+++ packages/web/src/shared/services/http.service.ts
@@ -2,11 +2,10 @@ import axios from 'axios';
 import { storageService } from './storage.service';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
